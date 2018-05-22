a801885e5354db12da3ba534b31a3eeea3aa5c07
Shravan Murali <shravanmurali@gmail.com>
2018-05-22 20:57:03 +0530

    Convert indentation to spaces

--- tests/test_node.py
+++ tests/test_node.py
@@ -3,93 +3,93 @@ import unittest
 
 ## Tests for Node class
 class OperationReadTest(unittest.TestCase):
-	def test_attr_read(self):		
-		node = Node(None, None)
-		func = node.Define
-		self.assertEqual(node._cur_attr, "Define")
+    def test_attr_read(self):       
+        node = Node(None, None)
+        func = node.Define
+        self.assertEqual(node._cur_attr, "Define")
 
-	def test_args_read(self):
-		node = Node(None, None)
-		newNode = node.Define(1, "b", a="1", b=2)
-		self.assertEqual(newNode.operation.args, (1, "b"))
+    def test_args_read(self):
+        node = Node(None, None)
+        newNode = node.Define(1, "b", a="1", b=2)
+        self.assertEqual(newNode.operation.args, (1, "b"))
 
-	def test_kwargs_read(self):
-		node = Node(None, None)
-		newNode = node.Define(1, "b", a="1", b=2)
-		self.assertEqual(newNode.operation.kwargs, {"a":"1", "b":2})
-		
+    def test_kwargs_read(self):
+        node = Node(None, None)
+        newNode = node.Define(1, "b", a="1", b=2)
+        self.assertEqual(newNode.operation.kwargs, {"a":"1", "b":2})
+
 class NodeReturnTest(unittest.TestCase):
-	def test_proxy_return(self):
-		node = Node(None, None)
-		newNode = node.Count()
-		self.assertIsInstance(newNode, Proxy)
+    def test_proxy_return(self):
+        node = Node(None, None)
+        newNode = node.Count()
+        self.assertIsInstance(newNode, Proxy)
 
-	def test_transformation_return(self):
-		node = Node(None, None)
-		newNode = node.Define(1)
-		self.assertIsInstance(newNode, Node)
+    def test_transformation_return(self):
+        node = Node(None, None)
+        newNode = node.Define(1)
+        self.assertIsInstance(newNode, Node)
 
 class DfsTest(unittest.TestCase):
-	class Temp(object):
-		def __init__(self):
-			self.ord_list = []
+    class Temp(object):
+        def __init__(self):
+            self.ord_list = []
 
-		def Define(self):
-			self.ord_list.append(1)
-			return self
+        def Define(self):
+            self.ord_list.append(1)
+            return self
 
-		def Filter(self):
-			self.ord_list.append(2)
-			return self
+        def Filter(self):
+            self.ord_list.append(2)
+            return self
 
-		def Count(self):
-			self.ord_list.append(3)
-			return self
-	
-	def test_dfs_order_small(self):
-		t = DfsTest.Temp()
-		
-		node = Node(None, None)
-		node.value = t
+        def Count(self):
+            self.ord_list.append(3)
+            return self
 
-		node._dfs(node = node._get_head())
+    def test_dfs_order_small(self):
+        t = DfsTest.Temp()
 
-		self.assertEqual(t.ord_list, [])
+        node = Node(None, None)
+        node.value = t
 
-	def test_dfs_order_big(self):
-		t = DfsTest.Temp()
+        node._dfs(node = node._get_head())
 
-		node = Node(None, None)
-		node.value = t
-		n1 = node.Define()
-		n2 = node.Filter()
-		n3 = n2.Filter()
-		n4 = n3.Count()
-		n5 = n1.Count()
-		n6 = node.Filter()
+        self.assertEqual(t.ord_list, [])
 
-		node._dfs(node = node._get_head())
+    def test_dfs_order_big(self):
+        t = DfsTest.Temp()
 
-		reqd_order = [1, 3, 2, 2, 3, 2]
+        node = Node(None, None)
+        node.value = t
+        n1 = node.Define()
+        n2 = node.Filter()
+        n3 = n2.Filter()
+        n4 = n3.Count()
+        n5 = n1.Count()
+        n6 = node.Filter()
 
-		self.assertEqual(t.ord_list, reqd_order)
+        node._dfs(node = node._get_head())
 
-	def test_dfs_graph_pruning(self):
-		t = DfsTest.Temp()
+        reqd_order = [1, 3, 2, 2, 3, 2]
 
-		node = Node(None, None)
-		node.value = t
-		n1 = node.Define()
-		n2 = node.Filter()
-		n3 = n2.Filter()
-		n4 = n3.Count()
-		n5 = n1.Count()
-		n6 = node.Filter()
+        self.assertEqual(t.ord_list, reqd_order)
 
-		n5 = n1.Filter()
+    def test_dfs_graph_pruning(self):
+        t = DfsTest.Temp()
 
-		node._dfs(node = node._get_head())
+        node = Node(None, None)
+        node.value = t
+        n1 = node.Define()
+        n2 = node.Filter()
+        n3 = n2.Filter()
+        n4 = n3.Count()
+        n5 = n1.Count()
+        n6 = node.Filter()
 
-		reqd_order = [1, 2, 2, 2, 3, 2]
+        n5 = n1.Filter()
 
-		self.assertEqual(t.ord_list, reqd_order)
+        node._dfs(node = node._get_head())
+
+        reqd_order = [1, 2, 2, 2, 3, 2]
+
+        self.assertEqual(t.ord_list, reqd_order)
--- tests/test_operation.py
+++ tests/test_operation.py
@@ -2,36 +2,36 @@ from PyTDF import *
 import unittest
 
 class ClassifyTest(unittest.TestCase):
-	def test_action(self):
-		op = Operation("Count")
-		self.assertEqual(op.op_type, "a")
+    def test_action(self):
+        op = Operation("Count")
+        self.assertEqual(op.op_type, "a")
 
-	def test_transformation(self):
-		op = Operation("Define", "c1")
-		self.assertEqual(op.op_type, "t")
+    def test_transformation(self):
+        op = Operation("Define", "c1")
+        self.assertEqual(op.op_type, "t")
 
-	def test_none(self):
-		op = Operation("random")
-		self.assertIs(op.op_type, None)
+    def test_none(self):
+        op = Operation("random")
+        self.assertIs(op.op_type, None)
 
 
 class ArgsTest(unittest.TestCase):
-	def test_without_kwargs(self):
-		op = Operation("Define", 1, "b")
-		self.assertEqual(op.args, (1, "b"))
-		self.assertEqual(op.kwargs, {})
+    def test_without_kwargs(self):
+        op = Operation("Define", 1, "b")
+        self.assertEqual(op.args, (1, "b"))
+        self.assertEqual(op.kwargs, {})
 
-	def test_without_args(self):
-		op = Operation("Define", a=1, b="b")
-		self.assertEqual(op.args, ())
-		self.assertEqual(op.kwargs, {"a":1, "b":"b"})
+    def test_without_args(self):
+        op = Operation("Define", a=1, b="b")
+        self.assertEqual(op.args, ())
+        self.assertEqual(op.kwargs, {"a":1, "b":"b"})
 
-	def test_with_args_and_kwargs(self):
-		op = Operation("Define", 2, "p", a=1, b="b")
-		self.assertEqual(op.args, (2, "p"))
-		self.assertEqual(op.kwargs, {"a":1, "b":"b"})
+    def test_with_args_and_kwargs(self):
+        op = Operation("Define", 2, "p", a=1, b="b")
+        self.assertEqual(op.args, (2, "p"))
+        self.assertEqual(op.kwargs, {"a":1, "b":"b"})
 
-	def test_without_args_and_kwargs(self):
-		op = Operation("Define")
-		self.assertEqual(op.args, ())
-		self.assertEqual(op.kwargs, {})
+    def test_without_args_and_kwargs(self):
+        op = Operation("Define")
+        self.assertEqual(op.args, ())
+        self.assertEqual(op.kwargs, {})
