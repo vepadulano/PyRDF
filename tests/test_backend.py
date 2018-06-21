9951875da238093db58827e6edfa58c09f6c1c9e
Shravan Murali <shravanmurali@gmail.com>
2018-06-21 21:21:16 +0530

    Add tests to check the working of abstract Backend class

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -38,3 +38,32 @@ class SelectionTest(unittest.TestCase):
 
         with self.assertRaises(NotImplementedError):
             PyRDF.use("dask")
+
+class BackendInitTest(unittest.TestCase):
+    """
+    Tests to ensure that the Backend abstract
+    class cannot be instantiated.
+
+    """
+    def test_backend_init_error(self):
+        """
+        Test case to check that any attempt to
+        instantiate the `Backend` abstract class
+        results in a `TypeError`.
+
+        """
+        with self.assertRaises(TypeError):
+            PyRDF.backend.Backend.Backend()
+
+    def test_subclass_without_method_error(self):
+        """
+        Test case to check that creation of a
+        subclass without implementing `execute`
+        method throws a `TypeError`.
+
+        """
+        class TestBackend(PyRDF.backend.Backend.Backend):
+            pass
+
+        with self.assertRaises(TypeError):
+            TestBackend()
