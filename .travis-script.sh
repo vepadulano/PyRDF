10f5c25a3ac6e03887d61157bd85f843f7a73787
Shravan Murali <shravanmurali@gmail.com>
2018-07-09 19:05:57 +0530

    Modify travis script to install pyspark and add more comments to script

--- .travis-script.sh
+++ .travis-script.sh
@@ -1,48 +1,57 @@
+# All apt-get installs
 sudo apt-get --yes update
 sudo apt-get --yes install python-pip
+sudo apt-get --yes install wget
+sudo apt-get --yes install default-jdk
+
+# Install required packages
 sudo pip install enum34
 sudo pip install nose
+
+# Pyspark installation
+mkdir /home/builder/spark
+cd /home/builder/spark
+wget http://www-eu.apache.org/dist/spark/spark-2.3.1/spark-2.3.1-bin-hadoop2.7.tgz
+tar -xvzf spark-2.3.1-bin-hadoop2.7.tgz
+cd spark-2.3.1-bin-hadoop2.7/python/
+sudo python setup.py install
+
+# Making ROOT available
 source /usr/local/bin/thisroot.sh
+
+# Change permissions of source code dir
 sudo chown -R $UID:$UID /app
-cd app
-python setup.py install --user
 
-nosetests
+# Install PyRDF from source
+cd /app
+sudo python setup.py install
 
+# Run tests
+nosetests tests/unit/*.py || exit 1
+nosetests tests/integration/local/*.py || exit 1
+
+# Run tutorials
 echo " ======== Running single-threaded tutorials ======== "
 
 # Run single-threaded tutorials locally
-{
-
 for filename in ./tutorials/local/sequential/df*.py
 do
 	echo " == Running $filename == "
-	python "$filename"
+	python "$filename" || exit 1
 	echo "  Ran $filename successfully ! "
 done
-
-} || {
-	echo "Error in running tutorials ! Check if you're in the PyRDF root directory !"
-	exit 1
-}
 
 echo " ======== Ran single-threaded tutorials successfully ! ======== "
 
 echo "======== Running multi-threaded tutorials ======== "
 
-{
 
 # Run multi-threaded tutorials locally
 for filename in ./tutorials/local/MT/df*.py
 do
 	echo " == Running $filename == "
-	python "$filename"
+	python "$filename" || exit 1
 	echo "  Ran $filename successfully ! "
 done
-
-} || {
-	echo "Error in running tutorials ! Check if you're in the PyRDF root directory !"
-	exit 1
-}
 
 echo " ======== Ran multi-threaded tutorials successfully ! ======== "
