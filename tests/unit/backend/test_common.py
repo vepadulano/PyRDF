da63df35927b0aadd18b57c7e33d7cea088d9e95
Shravan Murali <shravanmurali@gmail.com>
2018-07-24 00:10:38 +0530

    Add unit tests to check the working of 'declare_headers' method in Utils.py and 'include' method in PyRDF/__init__.py
    Add tests to check the headers include feature
    
    Modify unit tests to incorporate module changes and rename '.cpp' files to '.hxx'
    
    Invoke 'declare_headers' from Utils instead of Backend
    
    Clean up unit tests for 'includes' feature
    
    Explicitly include C++ string type in 'header2.hxx'

--- tests/unit/backend/test_common.py
+++ tests/unit/backend/test_common.py
@@ -1,4 +1,5 @@
 import PyRDF, unittest, ROOT
+from PyRDF.backend.Utils import Utils
 
 class SelectionTest(unittest.TestCase):
     """
@@ -45,3 +46,87 @@ class BackendInitTest(unittest.TestCase):
 
         with self.assertRaises(TypeError):
             TestBackend()
+
+class IncludeHeadersTest(unittest.TestCase):
+    """
+    Tests to check the working of 'PyRDF.include' function.
+
+    """
+    def tearDown(self):
+        """
+        Resets the module-level variable
+        'includes' to an empty list.
+
+        """
+        PyRDF.includes = [] # reset includes
+
+    def test_default_empty_list_include(self):
+        """
+        Test case to ensure that 'PyRDF.include' function
+        raises a TypeError if no parameter is given.
+
+        """
+        with self.assertRaises(TypeError):
+            PyRDF.include()
+
+    def test_string_include(self):
+        """
+        Test case to check the working of 'PyRDF.include'
+        function when a single string is passed to it.
+
+        """
+        PyRDF.include("header1")
+
+        self.assertListEqual(PyRDF.includes, ["header1"])
+
+    def test_list_include(self):
+        """
+        Test case to check the working of 'PyRDF.include'
+        function when a list of strings is passed to it.
+
+        """
+        PyRDF.include(["header1"])
+
+        self.assertListEqual(PyRDF.includes, ["header1"])
+
+    def test_list_extend_include(self):
+        """
+        Test case to check the working of 'PyRDF.include'
+        function when different lists of strings are passed
+        to it multiple times.
+
+        """
+        PyRDF.include(["header1", "header2"])
+        PyRDF.include(["header3", "header4", "header5"])
+
+        self.assertListEqual(PyRDF.includes, ["header1", "header2", "header3", "header4", "header5"])
+
+class DeclareHeadersTest(unittest.TestCase):
+    """
+    Tests to check the working of the static
+    method 'declare_headers' in Backend class.
+
+    """
+    def test_single_header_declare(self):
+        """
+        Test case to check the working of 'declare_headers'
+        function when a single header needs to be included.
+
+        """
+        Utils.declare_headers(["tests/unit/backend/test_headers/header1.hxx"])
+
+        self.assertEqual(ROOT.f(1), True)
+
+    def test_multiple_headers_declare(self):
+        """
+        Test case to check the working of 'declare_headers'
+        function when multiple headers need to be included.
+
+        """
+        Utils.declare_headers(["tests/unit/backend/test_headers/header1.hxx",
+                               "tests/unit/backend/test_headers/header2.hxx"
+                              ])
+
+        self.assertEqual(ROOT.f(1), True)
+        self.assertEqual(ROOT.f1(2), 2)
+        self.assertEqual(ROOT.f2("myString"), "myString")
