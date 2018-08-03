64fd39e5fc294bf21f5f9fb8666228de89ef0982
Shravan Murali <shravanmurali@gmail.com>
2018-08-03 16:36:25 +0530

    Add unit tests for GetValue method in Proxy

--- tests/unit/test_proxy.py
+++ tests/unit/test_proxy.py
@@ -1,6 +1,8 @@
 from PyRDF.Proxy import Proxy
 from PyRDF.Node import Node
-import unittest
+from PyRDF.backend.Backend import Backend
+from PyRDF import RDataFrame
+import unittest, PyRDF
 
 class AttrReadTest(unittest.TestCase):
     """
@@ -50,3 +52,56 @@ class AttrReadTest(unittest.TestCase):
         proxy = Proxy(node)
 
         self.assertEqual(proxy.val(21), 144)
+
+class GetValueTests(unittest.TestCase):
+    """
+    Test cases to check the working of
+    'GetValue' instance method in Proxy.
+
+    """
+    class TestBackend(Backend):
+        """
+        Test backend to verify the working of
+        'GetValue' instance method in Proxy.
+
+        """
+        def execute(self, generator):
+            """
+            Test implementation of the execute method
+            for 'TestBackend'. This records the head
+            node of the input PyRDF graph from the
+            generator object.
+
+            """
+            self.obtained_head_node = generator.head_node
+
+    def test_get_value_with_existing_value(self):
+        """
+        Test case to check the working of 'GetValue'
+        method in Proxy when the current action node
+        already houses a value.
+
+        """
+        node = Node(None, None)
+        proxy = Proxy(node)
+        node.value = 5
+
+        self.assertEqual(proxy.GetValue(), 5)
+
+    def test_get_value_with_value_not_existing(self):
+        """
+        Test case to check the working of 'GetValue'
+        method in Proxy when the current action node
+        doesn't contain a value (event-loop hasn't been
+        triggered yet).
+
+        """
+        PyRDF.current_backend = GetValueTests.TestBackend()
+
+        rdf = RDataFrame(10)
+        count = rdf.Count()
+
+        count.GetValue()
+
+        # Ensure that TestBackend's execute method was called
+        self.assertIs(PyRDF.current_backend.obtained_head_node, rdf)
