ba9b466f85c017c809d5663681dc79c29461905b
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2019-03-22 13:55:11 +0100

    Add better error handling for tutorials and increase verbosity of tests

--- .travis-script.sh
+++ .travis-script.sh
@@ -1,5 +1,25 @@
+#!/bin/bash
+
+# This script is meant to be called by the "install" step defined in
+# .travis.yml.
+
+check_error()
+{
+    local last_exit_code=$1
+    local last_cmd=$2
+    if [[ ${last_exit_code} -ne 0 ]]; then
+        echo "${last_cmd} exited with code ${last_exit_code}"
+        echo "TERMINATING TEST"
+        exit 1
+    else
+        echo "${last_cmd} completed successfully"
+    fi
+}
+
 # Run tests
-pytest
+# -x exit instantly on first error or failed test
+# -v increase verbosity
+pytest -x -v
 
 # Run tutorials
 echo " ======== Running single-threaded tutorials ======== "
@@ -8,34 +28,28 @@ echo " ======== Running single-threaded tutorials ======== "
 for filename in ./tutorials/local/sequential/df*.py
 do
 	echo " == Running $filename == "
-	python "$filename" || exit 1
-	echo "  Ran $filename successfully ! "
+	python "$filename" &> /dev/null
+  check_error $? "$filename"
 done
 
-echo " ======== Ran single-threaded tutorials successfully ! ======== "
 
 echo "======== Running multi-threaded tutorials ======== "
-
 
 # Run multi-threaded tutorials locally
 for filename in ./tutorials/local/MT/df*.py
 do
 	echo " == Running $filename == "
-	python "$filename" || exit 1
-	echo "  Ran $filename successfully ! "
+	python "$filename" &> /dev/null
+  check_error $? "$filename"
 done
 
-echo " ======== Ran multi-threaded tutorials successfully ! ======== "
 
 echo "======== Running Spark tutorials ======== "
-
 
 # Run Spark tutorials locally
 for filename in ./tutorials/spark/df*.py
 do
 	echo " == Running $filename == "
-	python "$filename" || exit 1
-	echo "  Ran $filename successfully ! "
+	python "$filename" &> /dev/null
+  check_error $? "$filename"
 done
-
-echo " ======== Ran Spark tutorials successfully ! ======== "
