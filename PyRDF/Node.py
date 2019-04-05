bda66bec8a45e4b3c7a01f9b9c4bccb94a68187c
Vincenzo Eduardo Padulano <v.e.padulano@gmail.com>
2019-04-05 16:08:40 +0200

    python version check in graph_prune() to define referrers threshold (#23)
    * added python version check in graph_prune() to define referrers threshold
    
    * added REFERRERS_THRESHOLD to docstrings

--- PyRDF/Node.py
+++ PyRDF/Node.py
@@ -2,13 +2,14 @@ from __future__ import print_function
 from PyRDF.Operation import Operation
 from PyRDF.Proxy import Proxy
 import gc
+import sys
 
 
 class Node(object):
     """
-    A Class that represents a node in RDataFrame operations graph. A Node houses
-    an operation and has references to children nodes. For details on the types
-    of operations supported, try :
+    A Class that represents a node in RDataFrame operations graph. A Node
+    houses an operation and has references to children nodes.
+    For details on the types of operations supported, try :
 
     import PyRDF
     PyRDF.use(...) # Choose your backend
@@ -17,8 +18,8 @@ class Node(object):
     Attributes
     ----------
     get_head : function
-            A lambda function that returns the head
-            node of the current graph.
+        A lambda function that returns the head
+        node of the current graph.
 
     operation
         The operation that this Node represents. This
@@ -39,7 +40,24 @@ class Node(object):
         Reference to the PyROOT object that implements the
         functionality of this node on the cpp side.
 
+    REFERRERS_THRESHOLD : class attribute
+        Minimum number of back references (objects pointing to current node)
+        for a node to be considered for pruning, used as a condition in
+        graph_prune() method. Python 2 keeps at least 3 references to a node:
+        one is due to the interpreter, another is the function call and the
+        last one is an internal reference which every object has. Python 3
+        only keeps the reference due to the interpreter. For this reason this
+        class attribute is different according to the Python version in use:
+        - Python 2 : REFERRERS_THRESHOLD = 3
+        - Python 3 : REFERRERS_THRESHOLD = 1
     """
+
+    if sys.version_info[0] >= 3:
+        # Python 3 specific definition
+        REFERRERS_THRESHOLD = 1
+    else:
+        # Python 2 specific definition
+        REFERRERS_THRESHOLD = 3
 
     def __init__(self, get_head, operation):
         """
@@ -187,13 +205,16 @@ class Node(object):
         if not self.children:
             # Every pruning condition is written on
             # a separate line
-            if len(gc.get_referrers(self)) <= 3 or \
+            if len(gc.get_referrers(self)) <= Node.REFERRERS_THRESHOLD or \
                (self.operation and self.operation.is_action() and self.value):
 
                 # ***** Condition 1 *****
+                #
+                # Python 2:
+                #
                 # If the number of referrers to the current node is
                 # less than 4, then the node has to be pruned.
-
+                #
                 # The 3 referrers to the current node would be :
                 # - The current function (graph_prune())
                 # - An internal reference (which every Python object has)
@@ -202,10 +223,15 @@ class Node(object):
                 # [If the user had a variable reference to the current node,
                 # the value would be at least 4. Hence nodes with less than
                 # 4 references will have to be removed ]
-
+                #
+                # Python 3:
+                #
+                # Here the minimum number of references a node can have is 1,
+                # due to the interpreter.
+                #
                 # NOTE :- sys.getrefcount(node) gives a way higher value and
                 # hence doesn't work in this case
-
+                #
                 # ***** Condition 2 *****
                 # If the current node's value was already
                 # computed, it should get pruned only if it's
